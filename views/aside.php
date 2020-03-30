3fe203fdb3dd81d9d7dfefee25f5838d7f2f4a96
François Kooman <fkooman@tuxed.net>
2020-03-30 21:48:18 +0200

    add ad for LC

--- views/aside.php
+++ views/aside.php
@@ -2,4 +2,8 @@
 <p class="latest">
 Our Latest Blog Post <a href="blog/<?php echo $this->e($latestBlog['fileName']); ?>"><?php echo $this->e($latestBlog['title']); ?></a>
 </p>
+
+<p class="lc">
+Not in the Research &amp; Education Community? Try <a href="https://www.letsconnect-vpn.org/">Let's Connect!</a>
+</p>
 <?php endif; ?>
